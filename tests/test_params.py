7826d18aba98d588f0036a713278c0312aedafd0
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-05-05 13:40:48 -0400

    add arg, dataclass, param system

---
+++ tests/test_params.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import dataclasses
+
+from cdkit.arg import REQ, OPT
+from cdkit.params import ConstructParams, StackParams
+
+
+@dataclasses.dataclass
+class MyConstructParams(ConstructParams):
+    name: str = dataclasses.field(default=REQ)
+    age: int = dataclasses.field(default=OPT)
+
+
+class TestConstructParams:
+    def test(self):
+        construct_params = MyConstructParams(
+            id="MyConstruct",
+            name="Alice",
+        )
+        assert construct_params.to_construct_kwargs() == {"id": "MyConstruct"}
+        assert construct_params.to_kwargs() == {"id": "MyConstruct", "name": "Alice"}
+
+
+@dataclasses.dataclass
+class MyStackParams(StackParams):
+    name: str = dataclasses.field(default=REQ)
+    age: int = dataclasses.field(default=OPT)
+
+
+class TestStackParams:
+    def test(self):
+        stack_params = StackParams(
+            id="MyStack",
+            stack_name="MyStackName",
+        )
+        kwargs = stack_params.to_stack_kwargs()
+        assert kwargs == {
+            "id": "MyStack",
+            "stack_name": "MyStackName",
+        }
+        assert stack_params.to_kwargs() == {
+            "id": "MyStack",
+            "stack_name": "MyStackName",
+        }
+
+
+if __name__ == "__main__":
+    from cdk_mate.tests import run_cov_test
+
+    run_cov_test(
+        __file__,
+        "cdkit.params",
+        preview=False,
+    )
